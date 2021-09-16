bc611a9f79dd85816091c2af108a3826a0318cf7
Nur Achmad Shafy Raissa Tsani <achmad.raissa@umk.ac.id>
2021-09-16 21:47:04 +0700

    .

---
+++ application/modules/partials/_head.php
@@ -0,0 +1,17 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Skydash Admin</title>
+<!-- plugins:css -->
+<link rel="stylesheet" href="<?php echo base_url('admin/vendors/feather/feather.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url('admin/vendors/ti-icons/css/themify-icons.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url('admin/vendors/css/vendor.bundle.base.css'); ?>">
+<!-- endinject -->
+<!-- Plugin css for this page -->
+<link rel="stylesheet" href="<?php echo base_url('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.css'); ?>">
+<link rel="stylesheet" href="<?php echo base_url('admin/vendors/ti-icons/css/themify-icons.css'); ?>">
+<link rel="stylesheet" type="text/css" href="<?php echo base_url('admin/js/select.dataTables.min.css'); ?>">
+<!-- End plugin css for this page -->
+<!-- inject:css -->
+<link rel="stylesheet" href="<?php echo base_url('admin/css/vertical-layout-light/style.css'); ?>">
+<!-- endinject -->
+<link rel="shortcut icon" href="<?php echo base_url('admin/images/favicon.png'); ?>">
---
+++ application/modules/partials/_js.php
@@ -0,0 +1,19 @@
+<script src="<?php echo base_url('admin/vendors/js/vendor.bundle.base.js'); ?>"></script>
+<!-- endinject -->
+<!-- Plugin js for this page -->
+<script src="<?php echo base_url('admin/vendors/chart.js/Chart.min.js'); ?>"></script>
+<script src="<?php echo base_url('admin/vendors/datatables.net/jquery.dataTables.js'); ?>"></script>
+<script src="<?php echo base_url('admin/vendors/datatables.net-bs4/dataTables.bootstrap4.js'); ?>"></script>
+<script src="<?php echo base_url('admin/js/dataTables.select.min.js'); ?>"></script>
+
+<!-- End plugin js for this page -->
+<!-- inject:js -->
+<script src="<?php echo base_url('admin/js/off-canvas.js'); ?>"></script>
+<script src="<?php echo base_url('admin/js/hoverable-collapse.js'); ?>"></script>
+<script src="<?php echo base_url('admin/js/template.js'); ?>"></script>
+<script src="<?php echo base_url('admin/js/settings.js'); ?>"></script>
+<script src="<?php echo base_url('admin/js/todolist.js'); ?>"></script>
+<!-- endinject -->
+<!-- Custom js for this page-->
+<script src="<?php echo base_url('admin/js/dashboard.js'); ?>"></script>
+<script src="<?php echo base_url('admin/js/Chart.roundedBarCharts.js'); ?>"></script>
